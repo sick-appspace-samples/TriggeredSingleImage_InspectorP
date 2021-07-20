d2fb12b1ab2f2ad181c34c2afad73d1e2cfc9e12
Mikolaj Rdzanek <mikolaj.rdzanek@sick.de>
2021-07-20 12:40:02 +0200

    'Update'

--- TriggeredSingleImage_InspectorP/scripts/TriggeredSingleImage.lua
+++ TriggeredSingleImage_InspectorP/scripts/TriggeredSingleImage.lua
@@ -1,21 +1,3 @@
---[[----------------------------------------------------------------------------
-
-  Application Name:
-  TriggeredSingleImage_InspectorP
-
-  Summary:
-  Setting up camera and taking single images by external trigger.
-
-  How to Run:
-  A connected InpspectorP device is necessary to run this sample. Starting this
-  sample is possible either by running the app (F5) or debugging (F7+F10).
-  Set a breakpoint on the first row inside the main or processImage function to debug step-by-step.
-  See the acquired images in the image viewer on the DevicePage.
-
-  More Information:
-  See the tutorial "Devices - InspectorP - TriggeringAndAcquisition".
-
-------------------------------------------------------------------------------]]
 
 --Start of Global Scope---------------------------------------------------------
 
